d54275cb596961924b95f882ef4f796f6e4ff5ef
peter frank <info@peterfrank.se>
2026-03-05 13:54:25 +0100

    Update reset.sh baseline to 2880734

--- reset.sh
+++ reset.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 git fetch origin
-git reset --hard 8511622
+git reset --hard 2880734
 git push origin main --force
-echo "Reset to baseline commit 8511622"
+echo "Reset to baseline commit 2880734"
